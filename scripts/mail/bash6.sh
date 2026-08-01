ca5fbb5d336c84b9738cb81ec343e0dd9832c254
Fares Belhaouas <fbelhaouas@gmail.com>
2026-08-01 15:22:25 -0400

    Update bash6.sh

--- scripts/mail/bash6.sh
+++ scripts/mail/bash6.sh
@@ -3,3 +3,13 @@ sudo bash -c 'target=/usr/local/bin/oosyroo-health; if [ -e "$target" ]; then cp
 sudo bash -n /usr/local/bin/oosyroo-health && echo "Syntax OK"
 
 sudo bash -c 'target=/usr/local/bin/oosyroo-health; cp -a -- "$target" "$target.backup-$(date +%Y%m%d-%H%M%S)" && chown root:root "$target" && chmod 0750 "$target" && stat -c "owner=%U group=%G mode=%a file=%n" "$target"'
+
+sudo /usr/local/bin/oosyroo-health --dry-run
+
+
+
+
+
+
+
+
